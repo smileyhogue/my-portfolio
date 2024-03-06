305c21d2fe23a5ab1430c1a1104100c995fe8325
Tristen <smileyhogue@users.noreply.github.com>
2024-03-06 17:55:58 -0500

    Update getResume.ts
    layoff

--- src/app/actions/getResume.ts
+++ src/app/actions/getResume.ts
@@ -4,7 +4,7 @@ import { revalidateTag } from 'next/cache';
 
 export default async function GetResume(): Promise<Object> {
   const resume = await fetch(
-    'https://gist.githubusercontent.com/smileyhogue/80c59982492aba1e5890cf3e305a4247/raw/4da6d8246f02a127b0b73db0d4958d50d27633f3/thogue.json',
+    'https://gist.githubusercontent.com/smileyhogue/80c59982492aba1e5890cf3e305a4247/raw/40c8314653ec8e14b3f6cbe5ce761f3b70f9a9aa/thogue.json',
     { next: { tags: ['resume'], revalidate: 3600 } }
   );
   const resumeData = JSON.parse(await resume.text());
